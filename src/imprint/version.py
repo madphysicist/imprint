abf3ff8292caa96e34285942ef33edb4e33427a5
Joseph R. Fox-Rabinovitz <jfoxrabinovitz@gmail.com>
2022-01-20 04:47:30 -0600

    MAINT/DOC: Bumped version to 0.1.0a2rc1 and added doc of procedure

--- src/imprint/version.py
+++ src/imprint/version.py
@@ -19,7 +19,7 @@
 
 # Author: Joseph Fox-Rabinovitz <jfoxrabinovitz at gmail dot com>
 # Version: 13 Apr 2019: Initial Coding
-
+# Version: 20 Jan 2022: Bumped version to 0.1.0a2rc1
 
 """
 :py:mod:`imprint.version` holds the version of the package.
@@ -28,7 +28,16 @@ The actual version number is normally exported through the top-level
 :py:mod:`imprint` package, but defined here. This module exists to
 facilitate deployment and testing, so users do not need to install the
 entire package to get the version number.
+
+Bump the version in the dev branch every time a prior version is tagged
+in master. Unless a major rewrite is planned, the new version should
+increment the minor number. The new version should be a release
+candidate, and have the corresponding `rc1` suffix. All following
+commits belong to the new version. Bump to full version immediately
+before merging with master. A tag is assigned on the master commit that
+merges in dev. Bump the version in dev immediately after merging master
+back in.
 """
 
 #: The current version
-__version__ = '0.1a1'
+__version__ = '0.1.0a2rc1'
